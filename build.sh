6aec8997db4e73df58ffc511652811e215daa928
Garth Henson <garth@guahanweb.com>
2017-04-10 20:59:38 -0700

    Adding docker push step

--- build.sh
+++ build.sh
@@ -3,7 +3,15 @@ PROJECT=$1;
 VERSION=$2;
 BUILD_DIR=$3;
 
+# log into docker
+echo "DOCKER: logging into registry..."
+login=$(docker login -u $SEGDS_REGISTRY_USERNAME -p $SEGDS_REGISTRY_PASSWORD segds-docker-registry.lns.starwave.com)
+
 # create docker image
 tag=$PROJECT":"$VERSION
 echo "DOCKER: building tag $tag..."
-res=$(docker build -t $tag $BUILD_DIR)
+res=$(docker build -t $PROJECT:$VERSION)
+
+# push docker image
+echo "DOCKER: pushing new image..."
+push=$(docker push $PROJECT)
