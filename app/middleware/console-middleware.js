4b8f57b3279342b3cbb26d3e361a93744563a68b
Burney, Kyle <Kyle.Burney@disney.com>
2017-04-12 11:22:19 -0700

    removed commented out code

--- app/middleware/console-middleware.js
+++ app/middleware/console-middleware.js
@@ -1,12 +1,8 @@
 'use strict';
 
-//const label_style = 'color: #4ab; font-weight: bold';
-//const timestamp_style = 'color: #aaa';
-
-module.exports = (/*{dispatch, getState }*/) => {
+module.exports = () => {
     return (next) => {
         return (action) => {
-            //console.info('%c[ACTION]%c(%s):%c State %o and action %o', label_style, timestamp_style, new Date(), 'color: #000', getState(), action);
             return next(action);
         };
     };
