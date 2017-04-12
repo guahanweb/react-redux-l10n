644e8fdb80a23a703905331488b723f046eb2124
Garth Henson <garth@guahanweb.com>
2017-04-12 13:04:55 -0700

    Updating pom with custom profiles

--- build.sh
+++ build.sh
@@ -1,18 +1,42 @@
 #!/usr/bin/env bash
-cmd=$1
+if test -t 1; then
+  ncolors=$(tput colors)
+
+  if test -n "$ncolors" && test $ncolors -ge 8; then
+    bold="$(tput bold)"
+    underline="$(tput smul)"
+    standout="$(tput smso)"
+    normal="$(tput sgr0)"
+    black="$(tput setaf 0)"
+    red="$(tput setaf 1)"
+    green="$(tput setaf 2)"
+    yellow="$(tput setaf 3)"
+    blue="$(tput setaf 4)"
+    magenta="$(tput setaf 5)"
+    cyan="$(tput setaf 6)"
+    white="$(tput setaf 7)"
+  fi
+fi
+
+function domsg() {
+    msg=$1
+    echo "${white}[BUILD] ${normal}${msg}"
+}
 
 case "$1" in
     build)
         tag=$2
-        echo "Building docker image..."
+        domsg "${cyan}building docker image"
         res=$(docker build -t $tag .)
         rc=$?
 
         if [[ $rc != 0 ]]; then
-            echo "ERROR ENCOUNTERED:"
+            domsg "${red}failure"
             echo $res
             exit $rc
         fi
+
+        domsg "${green}done"
         ;;
 
     push)
@@ -22,27 +46,29 @@ case "$1" in
         pass=$SEGDS_REGISTRY_PASSWORD
 
         # log in to secure docker registry
-        echo "Logging into registry..."
+        domsg "${cyan}logging into registry"
         res=$(docker login -u $SEGDS_REGISTRY_USERNAME -p $SEGDS_REGISTRY_PASSWORD $host)
         rc=$?
 
         if [[ $rc != 0 ]]; then
-            echo "COULD NOT LOG INTO $host"
+            domsg "${red}failure"
             echo $res
             exit $rc
         fi
 
-        echo "Pushing newly built image..."
+        domsg "${cyan}pushing docker image"
         res=$(docker push $tag)
         rc=$?
 
         if [[ $rc != 0 ]]; then
-            echo "COULD NOT PUSH IMAGE"
+            domsg "${red}failure"
             echo $res
             exit $rc
         fi
+
+        domsg "${green}done"
         ;;
     *)
-        echo $"Usage: $0 {build|push}"
+        domsg "${yellow}usage: $0 {build|push}"
         exit 1
 esac
